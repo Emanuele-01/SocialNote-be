1f6c2373f4291333989970b8b57eabf8b9b3dd18
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-06-27 11:47:24 +0200

    Update RegisterPayload
    creazione di un costruttore

--- SocialNote/src/main/java/Social/example/SocialNote/payload/RegisterPayload.java
+++ SocialNote/src/main/java/Social/example/SocialNote/payload/RegisterPayload.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterPayload {
+
 	@NotNull(message = "enter a valid name")
 	@Size(min = 3, max = 14, message = "There must be a minimum 3 characters and a maximum of 14")
 	 String name;
@@ -55,4 +56,25 @@ public class RegisterPayload {
 	LocalDate dayOfHiding;
 	
 // ---------------------------------------------------------------
+	
+	public RegisterPayload(
+			@NotNull(message = "enter a valid name") @Size(min = 3, max = 14, message = "There must be a minimum 3 characters and a maximum of 14") String name,
+			@NotNull(message = "enter a valid last name") @Size(min = 3, max = 14, message = "There must be a minimum 3 characters and a maximum of 14") String lastName,
+			@NotNull(message = "enter a valid username") @Size(min = 3, max = 15, message = "There must be a minimum 3 characters and a maximum of 15") String username,
+			@NotNull(message = "enter a valid email") @Size(min = 10, max = 25, message = "There must be a minimum 10 characters and a maximum of 25") String email,
+			@NotNull(message = "enter a valid password") @Size(min = 4, message = "There must be a minimum 4 characters") String password,
+			@NotNull(message = "enter a valid image Profile") String imageProfile,
+			@NotNull(message = "enter a valid age") @Size(min = 1, max = 3, message = "There must be a minimum 1 characters and a maximum of 3") int age,
+			@NotNull(message = "enter a valid day of hiding") LocalDate dayOfHiding) {
+		
+		super();
+		this.name = name;
+		this.lastName = lastName;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.imageProfile = imageProfile;
+		this.age = age;
+		this.dayOfHiding = dayOfHiding;
+	}
 }
